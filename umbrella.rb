eaeb0a742b9c85e622f65e1b2f2d9174e34218ce
annenyc <annelau.nyc@gmail.com>
2023-01-22 06:54:22 +0000

    Anne-Umbrella-last commit

--- umbrella.rb
+++ umbrella.rb
@@ -1,3 +1,5 @@
+https://gitpod.io#snapshot/3e889bcc-e651-48f3-9e73-d24014b053c2
+
 p "Where are you located?"
 
 #user_location = gets.chomp
@@ -6,7 +8,7 @@ user_location = "Detroit"
 
 p user_location
 
-p gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=AIzaSyB92cYxPcYqgjwBJfWlwDQw_7yjuyU3tpA"
+p gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{ENV.fetch("GMAPS_KEY")}"
 
 require("open-uri")
 
